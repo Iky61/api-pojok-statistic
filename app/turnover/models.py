c16fe6f08de2997d130454fbc47f2937ccb27e5d
Ryan Adi Putra <ryannadiputraa@gmail.com>
2022-12-23 17:32:13 +0800

    feat: upload template file

--- app/turnover/models.py
+++ app/turnover/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Turnover(models.Model):
-    file = models.CharField(max_length=100)
+    file = models.FileField(upload_to='turnover/uploads/')
 
     def __str__(self) -> str:
         return super().__str__()
