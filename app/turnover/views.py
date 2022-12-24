fa049902c3b0b5de92e024d4550c0c00a9dc5b0f
Dicky Aryanto <dickyaryanto@MacBook-Air-Dicky.local>
2022-12-24 10:44:26 +0800

    update feature data

--- app/turnover/views.py
+++ app/turnover/views.py
@@ -8,29 +8,66 @@ import pandas as pd
 
 
 class TurnoverApiView(APIView):
+    # fungsi untuk menentukan feature dan target
+    def Feature_Target(self, dataset):
+        df = dataset
+        
+        type_ = pd.DataFrame(df.dtypes).reset_index()
+        
+        objek = []
+        value = []
+        for x in range(len(type_)):
+            n = type_.iloc[x]
+            if n[0] == 'O' or n[0] == 'str':
+                objek.append(n['index'])
+            elif n[0] == 'int' or n[0] == 'float':
+                value.append(n['index'])
+            else:
+                None
+        
+        objek_value = []
+        for x in objek:
+            for y in value:
+                name = x + ' to ' + y
+                feature_target = [x, y]
+                a = {'kode':name, 'option':feature_target}
+                objek_value.append(a)
+                
+        return objek_value
+
+    # fungsi untuk mengirimkan data bar_plot
     def Bar_plot(self, filename):
         df = pd.read_excel(filename)
-        cols = df.columns.tolist()
-        feature = []
-        target = []
+    
+        final = []
+        all_obj_target = Feature_Target(dataset=df)
+        for x in all_obj_target:
+            feature = x['option'][0]
+            target = x['option'][1]
 
-        for x in cols:
-            if df[x].dtype == 'O':
-                feature.append(x)
-            else:
-                target.append(x)
+            df1 = df.groupby([feature])[[target]].sum().reset_index()
 
-        df = df.groupby(feature[0])[[target[0]]].sum().reset_index()
+            X = df1[feature].tolist()
+            Y = df1[target].tolist()
 
-        X = df[feature[0]].tolist()
-        Y = df[target[0]].tolist()
+            data = []
+            for a, b in zip(X, Y):
+                value = {'label':a, 'data':b}
+                data.append(value)
+                
+            result = {'Code':x['kode'], 'data':data}
+            final.append(result)
 
+<<<<<<< HEAD
         data = []
         for x, y in zip(X, Y):
             value = {'label': x, 'data': y}
             data.append(value)
 
         return data
+=======
+        return final
+>>>>>>> 1599c1c (mengubah feature)
 
     def post(self, request, *args, **kwargs):
         serializers = TurnoverSerializer(data=request.data)
